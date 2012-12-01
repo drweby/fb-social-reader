21ae95e22fd9b42a71da090105d0ebc34904d04c
Chris Houghton <chris.john.houghton@gmail.com>
2012-12-01 18:01:17 +0000

    Changed plugin name and details

--- fb-og-actions.php
+++ fb-og-actions.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: FB Social Reader
+Plugin Name: FB Social Reader BETA
 Plugin URI: http://fbsocialreader.com/
 Description: Transform your Wordpress into a social reading experience. With Social Reader, your readers automatically share articles on your site they read to Facebook, and can see what their friends read too.
 Version: 1.52
@@ -10,9 +10,9 @@ License: Regular
 */
 
 // Set constants
-define( 'FB_OG_PUGIN_NAME', 'FB Social Reader');
+define( 'FB_OG_PUGIN_NAME', 'FB Social Reader BETA');
 define( 'FB_OG_PLUGIN_URL', plugin_dir_url( __FILE__ ));
-define( 'FB_OG_CURRENT_VERSION', '1.52' );
+define( 'FB_OG_CURRENT_VERSION', '1.6' );
 
 // Include the functions file for functions that don't belong in classes
 include_once ('functions.php');
